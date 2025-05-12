5a7b9c1789d56179c1331f863eafe5b9b1de8314
aclementcl <alejandro.clemente.i@gmail.com>
2025-05-12 16:40:13 -0400

    Connecting webapi to infraestrcuture

---
+++ src/Club.Application/Services/MemberService.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Club.Application.Services;
+
+public class MemberService
+{
+    // private readonly ClubContext _context;
+
+    // public MemberService(ClubContext context)
+    // {
+    //     _context = context;
+    // }
+}
--- src/Club.Webapi/Controllers/MemberController.cs
+++ src/Club.Webapi/Controllers/MemberController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ClubApp.Data;
-using ClubApp.Models;
+using Club.Infrastructure.Data;
+using Club.Infrastructure.Models;
 
 namespace ClubApp.Controllers;
 
